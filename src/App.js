83b10ffbfac3d5c65c9a933a038838cbcafa69d3
Juan Antonio Ceballos <jacu29@gmail.com>
2021-06-25 13:45:30 -0500

    App que realiza la conversión de grados a radianes y viceversa

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Conversor from './components/Conversor'
+
+function App() {
+  return (
+    <div className="App">
+      <Conversor />
+    </div>
+  );
+}
+
+export default App;
